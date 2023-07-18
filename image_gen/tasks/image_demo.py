6694bd1c1305cc7d367cb742fb48ec97ed8930f2
Benjamin Bolte <ben@bolte.cc>
2023-07-17 23:32:46 -0400

    Initialize project

--- image_gen/tasks/image_demo.py
+++ image_gen/tasks/image_demo.py
@@ -18,7 +18,7 @@ import torch.nn.functional as F
 import torchvision
 from torch import Tensor
 
-from project.models.resnet import ResNetModel
+from image_gen.models.resnet import ResNetModel
 
 CLASS_TO_IDX = {
     "airplane": 0,
--- setup.py
+++ setup.py
@@ -17,18 +17,18 @@ with open("requirements-dev.txt", "r", encoding="utf-8") as f:
     requirements_dev: list[str] = f.read().splitlines()
 
 
-with open("<name>/__init__.py", "r", encoding="utf-8") as fh:
+with open("image_gen/__init__.py", "r", encoding="utf-8") as fh:
     version_re = re.search(r"^__version__ = \"([^\"]*)\"", fh.read(), re.MULTILINE)
-assert version_re is not None, "Could not find version in <name>/__init__.py"
+assert version_re is not None, "Could not find version in image_gen/__init__.py"
 version: str = version_re.group(1)
 
 
 setup(
-    name="<name>",
+    name="image_gen",
     version=version,
-    description="<description>",
-    author="<author>",
-    url="<url>",
+    description="Image generation model experiments",
+    author="Benjamin Bolte",
+    url="https://github.com/codekansas/image-generation",
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.10",
